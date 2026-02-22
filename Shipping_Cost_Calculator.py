278a7c78d91b617fb30f290d77bb278e52874608
crbuzaeanu <crbuzaeanu@gmail.com>
2026-02-22 18:14:32 +0200

    Revert "added additional line to Shipping_Cost_Calculator.py"
    This reverts commit d671dfd9731216b2a4b0c0dd287b4823b7cedd96.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -1,5 +1,4 @@
-# Here is another update by crbuzaeanu
-# Here is a new update by crbuzaeanu
+ # Here is a new update by crbuzaeanu
 # Shipping Cost Calculator
 
 ## Input package weight and shipping rate
